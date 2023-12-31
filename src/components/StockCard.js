49e983daff58dc5177fd80a25eb25920705b2957
ujjwal patel <newujjwalpatel@gmail.com>
2023-12-31 21:26:18 +0530

    responsive for all part 3

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -11,37 +11,37 @@ export const StockCard = (props) => {
    <ReactCardFlip isFlipped={flip}
    flipDirection="horizontal">
    <div className='shadow-md shadow-blue-300 hover:shadow-xl hover:shadow-yellow-300'>
-   <div onClick={() => setFlip(!flip)} className={` sm:text-pretty rounded-lg border-2 border-black grid gap-6 grid-cols-3  ${props.stock.buyOrNot==="buy"?"bg-green-400":"bg-red-400"} hover:scale-110`}  >
+   <div onClick={() => setFlip(!flip)} className={`  rounded-lg border-2 border-black grid gap-6 grid-cols-3  ${props.stock.buyOrNot==="buy"?"bg-green-400":"bg-red-400"} hover:scale-110`}  >
    
    <div className='text-3xl  text-white col-span-3 text-left mx-2 my-2'>{props.stock.index}: {props.stock.name}
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>Price:
+    <div className=' text-gray-200 text-left m-2 text-base'>Price:
        <div className='text-black'>Rs.{props.stock.currentPrice}
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>High:
+    <div className='  text-gray-200 text-left m-2 text-base'>High:
        <div className=' text-black   '>Rs.{props.stock.highestPrice}
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>Low:
+    <div className=' text-gray-200 text-left m-2 text-base'>Low:
        <div className=' text-black   '>Rs.{props.stock.lowestPrice}
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>Roe:
+    <div className=' text-gray-200 text-left m-2 text-base'>Roe:
        <div className='text-black'>{props.stock.roe}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>Roce:
+    <div className='  text-gray-200 text-left m-2 text-base'>Roce:
        <div className=' text-black    '>{props.stock.roce}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>Pe:
+    <div className='  text-gray-200 text-left m-2 text-base'>Pe:
        <div className=' text-black   '>{props.stock.pe}%
        </div>
     </div>
@@ -55,32 +55,32 @@ export const StockCard = (props) => {
    <div className={`text-3xl   col-span-3 text-left mx-2 ${props.stock.growthPeriod.startyear===0?"text-red-800":"text-green-800"}`}>{`${props.stock.growthPeriod.startyear===0 ? "Doesn't have a growth rate":` ${props.stock.growthPeriod.startyear} to ${props.stock.growthPeriod.endyear} by ${props.stock.growthPeriod.growth.toFixed(0)}%`}`}
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>cp10:
+    <div className='text-base  text-gray-200 text-left m-2 '>cp10:
        <div className='text-black'>{props.stock.cp10}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>cp5:
+    <div className='text-base  text-gray-200 text-left m-2 '>cp5:
        <div className=' text-black   '>{props.stock.cp5}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>cp3:
+    <div className='text-base  text-gray-200 text-left m-2 '>cp3:
        <div className=' text-black   '>{props.stock.cp3}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>csg10:
+    <div className='text-base  text-gray-200 text-left m-2 '>csg10:
        <div className='text-black'>{props.stock.csg10}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>csg5:
+    <div className='text-base  text-gray-200 text-left m-2 '>csg5:
        <div className=' text-black   '>{props.stock.csg5}%
        </div>
     </div>
 
-    <div className='text-xl  text-gray-200 text-left m-2 '>csg3:
+    <div className='text-base  text-gray-200 text-left m-2 '>csg3:
        <div className=' text-black   '>{props.stock.csg3}%
        </div>
     </div>
